7dab1a89608207eb3903a94e9b5abf56db942556
Keeya Emmanuel <keeyaemmanuel12@gmail.com>
2022-08-04 18:52:24 +0300

    few changes

--- app/src/main/java/com/dsmagic/kibira/utils/GeneralHelper.java
+++ app/src/main/java/com/dsmagic/kibira/utils/GeneralHelper.java
@@ -9,8 +9,6 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import dilivia.s2.S2LatLng;
 import dilivia.s2.index.point.S2PointIndex;
@@ -18,7 +16,7 @@ import dilivia.s2.index.point.S2PointIndex;
 public class GeneralHelper {
 
     public static S2PointIndex<S2LatLng> convertLineToS2(Collection<LatLng> list){
-        S2PointIndex querySet = new S2PointIndex<S2LatLng>();
+        S2PointIndex<S2LatLng> querySet = new S2PointIndex<S2LatLng>();
        for (LatLng x : list){
            S2LatLng s2LatLng = S2LatLng.fromDegrees(x.latitude, x.longitude);
            querySet.add(s2LatLng.toPoint(),s2LatLng );
